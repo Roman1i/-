8e3c3db16dd30dc4c3123612c4c35c0884532c83
Roman <igoshinr.estho@gmail.com>
2022-09-09 11:01:05 +0300

    commit1

---
+++ задача/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите колличество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+string[] strings = new string[n];
+
+for (int i = 0; i < n; i++) 
+    {
+        Console.Write("Введите строку: ");
+        strings[i] = Console.ReadLine();
+        if ( strings[i].Length <= 3 ) count++;
+    }
+    
+Console.WriteLine("[" + string.Join(", ", strings) + "]");
