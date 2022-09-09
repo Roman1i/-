2567892b1d489fcbae1cd80d742b6f63c5e96757
Roman <igoshinr.estho@gmail.com>
2022-09-09 11:11:23 +0300

    commit2

--- задача/Program.cs
+++ задача/Program.cs
@@ -5,10 +5,22 @@ int count = 0;
 string[] strings = new string[n];
 
 for (int i = 0; i < n; i++) 
-    {
-        Console.Write("Введите строку: ");
-        strings[i] = Console.ReadLine();
-        if ( strings[i].Length <= 3 ) count++;
-    }
+{
+    Console.Write("Введите строку: ");
+    strings[i] = Console.ReadLine();
+    if ( strings[i].Length <= 3 ) count++;
+}
     
 Console.WriteLine("[" + string.Join(", ", strings) + "]");
+
+string[] strings2 = new string[count];
+int x = 0;
+
+for (int i = 0; i < n; i++) 
+{
+    if ( strings[i].Length <= 3 ) 
+        strings2[x++] = strings[i];
+}
+
+Console.WriteLine("[" + string.Join(", ", strings2) + "]");
+
